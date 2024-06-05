879391828fcca725aa628045d12d71e72c984c6e
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-05 17:44:48 -0300

    atualização da interface conta

--- BancoDigital/src/Repository/IConta.java
+++ BancoDigital/src/Repository/IConta.java
@@ -8,4 +8,8 @@ public interface IConta {
     void transferir(double valor, IConta contaDestino);
 
     void solicitarEmprestimo(double valor);
+
+    void imprimirDadosConta();
+
+    void imprimirInfosConta();
 }
