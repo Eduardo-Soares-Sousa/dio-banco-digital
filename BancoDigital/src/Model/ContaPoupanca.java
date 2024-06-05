8f5a35789a4a1f36345ab615ea44820b1130e5b7
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-05 13:31:19 -0300

    conta poupança extende de Conta

--- BancoDigital/src/Model/ContaPoupanca.java
+++ BancoDigital/src/Model/ContaPoupanca.java
@@ -1,2 +1,10 @@
-package Model;public class ContaPoupanca {
+package Model;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(String nomeBanco, int agencia, int numeroConta, double saldo) {
+        super(nomeBanco, agencia, numeroConta, saldo);
+    }
+
+    
 }
