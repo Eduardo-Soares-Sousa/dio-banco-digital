7bbff96d2ced910a0680824f4bbfecaa5cc64d27
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-05 17:47:10 -0300

    atualização da classe conta

--- BancoDigital/src/Model/Conta.java
+++ BancoDigital/src/Model/Conta.java
@@ -2,7 +2,7 @@ package Model;
 
 import Repository.IConta;
 
-public class Conta implements IConta {
+public abstract class Conta implements IConta {
     private static final String NOME_BANCO = "North Bank";
     private static final int NUMERO_AGENCIA = 4041;
     private static int CONTA_SEQUENCIAL = 1;
@@ -11,31 +11,56 @@ public class Conta implements IConta {
     private int agencia;
     private int numeroConta;
     private double saldo;
+    protected Cliente cliente;
 
-    public Conta(String nomeBanco, int agencia, int numeroConta, double saldo) {
+    public Conta() {
         this.nomeBanco = NOME_BANCO;
         this.agencia = NUMERO_AGENCIA;
         this.numeroConta = CONTA_SEQUENCIAL++;
-        this.saldo = saldo;
+        this.saldo = 0.0;
     }
 
     @Override
     public void sacar(double valor) {
-
+        saldo -= valor;
     }
 
     @Override
     public void depositar(double valor) {
-
+        saldo += valor;
     }
 
     @Override
     public void transferir(double valor, IConta contaDestino) {
-
+        this.sacar(valor);
+        contaDestino.depositar(valor);
     }
 
     @Override
     public void solicitarEmprestimo(double valor) {
 
+    }
+
+    public String getNomeBanco() {
+        return nomeBanco;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void imprimirInfosConta() {
+        System.out.println("Nome do Banco: " + this.nomeBanco);
+        System.out.println("Número da Agencia: " + this.agencia);
+        System.out.println("Número da Conta: " + this.numeroConta);
+        System.out.println("Saldo disponível: " + this.saldo);
     }
 }
