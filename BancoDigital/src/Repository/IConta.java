b0ac55be741a061768dfd5bce831a48fca602dd1
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-05 12:40:38 -0300

    Interface de conta

--- BancoDigital/src/Repository/IConta.java
+++ BancoDigital/src/Repository/IConta.java
@@ -1,2 +1,11 @@
-package Repository;public interface ContaInterface {
+package Repository;
+
+public interface IConta {
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor, IConta contaDestino);
+
+    void solicitarEmprestimo(double valor);
 }
