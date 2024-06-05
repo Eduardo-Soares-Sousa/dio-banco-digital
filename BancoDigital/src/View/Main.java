185a07660cb8d89453c44f5b81d474452203003d
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-05 17:42:54 -0300

    Otimização do método main

--- BancoDigital/src/View/Main.java
+++ BancoDigital/src/View/Main.java
@@ -1,17 +1,90 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+package View;
+
+import Model.TipoDaConta;
+import Service.Banco;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Banco banco = new Banco();
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("\tBem-vindo ao North Bank!");
+        boolean sair = false;
+        while(sair != true){
+            System.out.println("\n");
+            System.out.println("\t\tDigite a opcao desejada.");
+            System.out.println("\t1 - Adicionar um novo cliente: ");
+            System.out.println("\t2 - Remover um cliente: ");
+            System.out.println("\t3 - Consultar os dados de um cliente: ");
+            System.out.println("\t4 - Exibir os clientes cadastrados: ");
+            System.out.println("\t5 - Exibir quantos clientes estão cadastros: ");
+            System.out.println("\t6 - Exibir dados da conta de um cliente: ");
+            System.out.println("\t0 - Sair");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+            switch(opcao){
+                case 1:
+                    System.out.println("\tAdicionar um novo cliente");
+                    System.out.println("Digite o nome do cliente: ");
+                    String nome = scanner.nextLine();
+                    System.out.println("Digite o cpf do cliente: ");
+                    String cpf = scanner.nextLine();
+                    System.out.println("Digite a idade do cliente: ");
+                    int idade = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Digite o qual o tipo da conta (CORRENTE ou POUPANCA): ");
+                    String tipoDaContaBanco = scanner.nextLine();
+                    TipoDaConta tipoDaConta = null;
+
+                    try{
+                        tipoDaConta = TipoDaConta.valueOf(tipoDaContaBanco.toUpperCase());
+                    }catch(IllegalArgumentException e){
+                        System.out.println("Tipo da conta inválido. Por favor, digite CORRENTE ou POUPANCA.");
+                        break;
+                    }
+
+                    banco.adicionarCliente(nome, cpf, idade, tipoDaConta);
+                    break;
+                case 2:
+                    System.out.println("\tRemover um cliente");
+                    System.out.println("Digite o cpf do cliente que será removido: ");
+                    String cpf1 = scanner.nextLine();
+
+                    banco.removerCliente(cpf1);
+                    break;
+                case 3:
+                    System.out.println("\tConsultar os dados de um cliente");
+                    System.out.println("Digite o cpf do cliente que deseja fazer a consulta: ");
+                    String cpf2 = scanner.nextLine();
+
+                    banco.pesquisarCliente(cpf2);
+                    break;
+                case 4:
+                    System.out.println("\tClientes cadastrados: ");
+                    banco.exibirClientes();
+                    break;
+                case 5:
+                    System.out.println("\tExibir quantos clientes estão cadastros");
+                    System.out.println("O sistema possui " + banco.contarClientes() + " clientes cadastrados!");
+                    break;
+                case 6:
+                    System.out.println("\tExibir dados da conta de um cliente");
+                    System.out.println("Digite o cpf do cliente que deseja consultar: ");
+                    String cpf3 = scanner.nextLine();
+
+                    banco.dadosContaCliente(cpf3);
+                    break;
+                case 0:
+                    sair = true;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
         }
+        System.out.println("Sistema encerrado!");
     }
 }
