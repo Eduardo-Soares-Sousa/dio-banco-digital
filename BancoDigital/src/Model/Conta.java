34d99ab118914e270a8bc54b8564db893092508c
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-05 13:27:45 -0300

    Implementa a interface IConta

--- BancoDigital/src/Model/Conta.java
+++ BancoDigital/src/Model/Conta.java
@@ -1,2 +1,41 @@
-package Model;public class Conta {
+package Model;
+
+import Repository.IConta;
+
+public class Conta implements IConta {
+    private static final String NOME_BANCO = "North Bank";
+    private static final int NUMERO_AGENCIA = 4041;
+    private static int CONTA_SEQUENCIAL = 1;
+
+    private String nomeBanco;
+    private int agencia;
+    private int numeroConta;
+    private double saldo;
+
+    public Conta(String nomeBanco, int agencia, int numeroConta, double saldo) {
+        this.nomeBanco = NOME_BANCO;
+        this.agencia = NUMERO_AGENCIA;
+        this.numeroConta = CONTA_SEQUENCIAL++;
+        this.saldo = saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+
+    }
+
+    @Override
+    public void solicitarEmprestimo(double valor) {
+
+    }
 }
