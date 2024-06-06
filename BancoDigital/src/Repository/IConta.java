7da296867c0a0dd5d080f276a996df4d27c2db32
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-06 12:08:00 -0300

    Término da interface de conta

--- BancoDigital/src/Repository/IConta.java
+++ BancoDigital/src/Repository/IConta.java
@@ -1,11 +1,13 @@
 package Repository;
 
+import Model.Conta;
+
 public interface IConta {
     void sacar(double valor);
 
     void depositar(double valor);
 
-    void transferir(double valor, IConta contaDestino);
+    void transferir(double valor, Conta contaDestino);
 
     void solicitarEmprestimo(double valor);
 
