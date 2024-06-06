88173584c93d1129e1dbed6355f188ef53bc0701
Eduardo Soares <eduardo_soares001@outlook.com>
2024-06-06 12:06:46 -0300

    Atualização da classe conta

--- BancoDigital/src/Model/Conta.java
+++ BancoDigital/src/Model/Conta.java
@@ -22,23 +22,53 @@ public abstract class Conta implements IConta {
 
     @Override
     public void sacar(double valor) {
-        saldo -= valor;
+        if(valor < saldo){
+            saldo -= valor;
+            System.out.println("Saque efetuado com sucesso.");
+        }else{
+            System.out.println("Impossível realizar o saque. O valor a ser sacado supera seu saldo.");
+        }
     }
 
     @Override
     public void depositar(double valor) {
-        saldo += valor;
+        if(valor > 0){
+            saldo += valor;
+            System.out.println("Deposito efetuado com sucesso.");
+        }else{
+            System.out.println("Impossível realizar o deposito. O valor que deseja depositar não pode ser negativo.");
+        }
     }
 
     @Override
-    public void transferir(double valor, IConta contaDestino) {
-        this.sacar(valor);
-        contaDestino.depositar(valor);
+    public void transferir(double valor, Conta contaDestino) {
+        if(valor < saldo && valor <= saldo){
+            this.sacar(valor);
+            contaDestino.depositar(valor);
+            System.out.println("Transferência realizada com sucesso.");
+        }else if(valor < 0){
+            System.out.println("Impossível realizar a transferência. O valor solicitado não pode ser negativo.");
+        }else{
+            System.out.println("Impossível realizar a transferência. O valor solicitado supera seu saldo.");
+        }
     }
 
     @Override
     public void solicitarEmprestimo(double valor) {
+        if(valor < 10000.00){
+            this.depositar(valor);
+            System.out.println("Você tem 1 ano e 6 messes para pagar o empréstimo, com acrescimo de 40% do valor retirado.");
+            System.out.println("O valor a ser pago será de: R$ " + valorPagoEmprestimo(valor));
+        }else if(valor > 10000.00){
+            System.out.println("Impossível realizar o empréstimo. O valor solicitado é superior ao limite de R$ 10.000,00.");
+        }else{
+            System.out.println("Impossível realizar o empréstimo. O valor solicitado não pode ser negativo.");
+        }
+    }
 
+    public double valorPagoEmprestimo(double valor){
+        double totalPago = valor;
+        return totalPago += ((valor * 40) / 100);
     }
 
     public String getNomeBanco() {
